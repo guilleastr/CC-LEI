dfacb307064211ffd8b8d8366cc0286825a47f75
uo269450 <UO269450@uniovi.es>
2021-12-09 10:21:44 +0000

    Pacakge Schematics

---
+++ packages/types/AcknowlegementPacakge.java
@@ -0,0 +1,16 @@
+package packages.types;
+
+public class AcknowlegementPacakge extends Base_Package{
+
+    public AcknowlegementPacakge(int type) {
+        super(type);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Void execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
---
+++ packages/types/DataPacakge.java
@@ -0,0 +1,16 @@
+package packages.types;
+
+public class DataPacakge extends Base_Package{
+
+    public DataPacakge(int type) {
+        super(type);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Void execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
