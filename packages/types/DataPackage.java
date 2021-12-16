d5868a3ad5a4dcacb67efd3c8f6f54978238ba54
ruipgcoelho <60889549+ruipgcoelho@users.noreply.github.com>
2021-12-16 10:06:56 +0000

    Update DataPackage.java
    added code for data packet

--- packages/types/DataPackage.java
+++ packages/types/DataPackage.java
@@ -1,19 +1,76 @@
 package packages.types;
 
+import java.util.Objects;
+
 public class DataPackage extends Base_Package{
 
-    private int dataSegment;
-    
+    private short sizeSegment;
+    private int segmentation;
+    private String data;
+
+
 
     public DataPackage(int type) {
         super(type);
-        //TODO Auto-generated constructor stub
+        this.sizeSegment = 0;
+        this.segmentation = 0;
+        this.data = "";
+    }
+
+    public DataPackage(int type, short sizeSegment, int segmentation, String data){
+        super(type);
+        this.sizeSegment = sizeSegment;
+        this.segmentation = segmentation;
+        this.data = data;
     }
 
     @Override
     public Void execute() {
-        // TODO Auto-generated method stub
+        System.out.println(sizeSegment);
+        System.out.println(segmentation);
+        System.out.println(data);
         return null;
     }
-    
+
+    public short getSizeSegment() {
+        return sizeSegment;
+    }
+
+    public void setSizeSegment(short sizeSegment) {
+        this.sizeSegment = sizeSegment;
+    }
+
+    public int getSegmentation() {
+        return segmentation;
+    }
+
+    public void setSegmentation(int segmentation) {
+        this.segmentation = segmentation;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DataPackage that = (DataPackage) o;
+        return sizeSegment == that.sizeSegment && segmentation == that.segmentation && Objects.equals(data, that.data);
+    }
+
+    @Override
+    public String toString() {
+        return "DataPackage{" +
+                "type=" + type +
+                ", sizeSegment=" + sizeSegment +
+                ", segmentation=" + segmentation +
+                ", data='" + data + '\'' +
+                '}';
+    }
 }
