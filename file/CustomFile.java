351695b8189c22d4f93da950aeb9db28604e7650
guilleastr <guillermoastorgamanzanal@gmail.com>
2021-12-21 21:47:13 +0100

    Added time to CustomFile

--- file/CustomFile.java
+++ file/CustomFile.java
@@ -7,18 +7,35 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Abstraction of File. For easly calculatin MD5 hash and Date.
+ *
+ */
 public class CustomFile {
 
 	private File file;
 
 	private String md5;
+	private long time;
 
 	public CustomFile(File file) {
 		super();
 		this.file = file;
 		generateMD5();
+		this.time = file.lastModified();
+
 	}
 
+	public CustomFile(String string) {
+		String filename = string.split(",")[0];
+		this.file = DirectoryManagerSingleton.getInstance().getFile(filename);
+		this.md5 = string.split(",")[1].split("/")[0].stripTrailing();
+		this.time = Long.parseLong(string.split(",")[1].split("/")[1].stripTrailing());
+	}
+
+	/**
+	 * Generates a MD5 hash and sets it to a class variable
+	 */
 	private void generateMD5() {
 		MessageDigest md;
 		byte[] messageDigest = null;
@@ -26,11 +43,11 @@ public class CustomFile {
 			md = MessageDigest.getInstance("MD5");
 			// digest() method is called to calculate message digest
 			// of an input digest() return array of byte
-			FileInputStream fis=new FileInputStream(file);
+			FileInputStream fis = new FileInputStream(file);
 			messageDigest = md.digest(fis.readAllBytes());
 			fis.close();
 		} catch (NoSuchAlgorithmException | IOException e) {
-			
+
 			e.printStackTrace();
 		}
 
@@ -46,27 +63,22 @@ public class CustomFile {
 
 	}
 
-	private File getFile() {
-		return file;
-	}
-
-	private String getMd5() {
-		return md5;
-	}
-
 	public void setMd5(String md5) {
 		this.md5 = md5;
 	}
 
 	@Override
 	public String toString() {
-		return file.getName() + "," + md5 ;
-	}
-	
-	public String toStringFormat() {
-		return file.getName() + "," + md5 + "\n";
+		return "FILE: "+ file.getName() + "; " + md5+"; TIME: "+time;
 	}
 
-	
+	/**
+	 * Returns the string that will be inserted into a data section package
+	 * 
+	 * @return
+	 */
+	public String toStringFormat() {
+		return file.getName() + "," + md5 + "/" + file.lastModified() + "\n";
+	}
 
 }
